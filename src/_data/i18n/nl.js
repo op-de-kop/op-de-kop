6d658859baba0377084000fa01662aa1abe3bb45
Rian Ahsmann <rian@farm21.tech>
2022-06-02 14:02:04 +0200

    Fix more translations and styling

--- src/_data/i18n/nl.js
+++ src/_data/i18n/nl.js
@@ -3,4 +3,9 @@ module.exports = {
     terms_and_conditions: "lesvoorwaarden",
     question: "vraag",
     answer: "antwoord",
+    op_de_kop: "Op De Kop is de acrobatiekvereniging van Utrecht opgericht door een 8-tal acrobaten. Er zijn wekelijkse lessen. De lessen worden gegeven door ervaren docenten.",
+    lessons_for_everyone: "De lessen zijn voor iedereen vanaf 16 jaar. Je hoeft géén ervaring met acrobatiek te hebben en je hoeft ook niet sterk of lenig te zijn, als je meedoet met de lessen komt dat vanzelf.",
+    interested: "Als je geïnteresseerd bent in de lessen laat het ons dan weten via het formulier",
+    subscribe: "Inschrijven"
+
 };
